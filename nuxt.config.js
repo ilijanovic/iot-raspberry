d00bcb2b87c05dcbe6f1350020bdaabdb8e49393
ilijanovic <ilija.marijanovic@gmx.at>
2020-09-24 15:36:20 +0200

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -32,12 +32,7 @@ export default {
       },
     ],
   },
-  env: {
-    USER: 'ifaruki',
-    PASSWORD: 'iotrasp122',
-    SERVER: 'cluster0.va95b.mongodb.net',
-    COLLECTION: 'iot',
-  },
+
   /*
    ** Global CSS
    */
