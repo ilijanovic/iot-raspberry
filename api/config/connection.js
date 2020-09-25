8bb03de4012b22eb285c6f8848049a6acd2ac488
Ilija Marijanovic <ilija.marijanovic@gmx.at>
2020-09-25 11:47:39 +0200

    Update connection.js

--- api/config/connection.js
+++ api/config/connection.js
@@ -1,3 +1,8 @@
+/**
+ * Creates a new connection to the database
+ */
+
+
 import mongoose from 'mongoose'
 export const conn = mongoose.createConnection(process.env.MONGODB_URI, {
   useNewUrlParser: true,
