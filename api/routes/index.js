acb647e44c399d9e51f8dcfb0e89fd5415f897d4
Ilija Marijanovic <ilija.marijanovic@gmx.at>
2020-09-25 11:20:53 +0200

    Update index.js

--- api/routes/index.js
+++ api/routes/index.js
@@ -5,6 +5,10 @@ import { loginHandler } from '../controllers/login'
 import { registerHandler } from '../controllers/register'
 import { auth } from '../controllers/auth'
 
+// Route: /api/login
 router.post('/login', loginHandler)
+
+// Route: /api/register
 router.post('/register', registerHandler)
+
 export default router
