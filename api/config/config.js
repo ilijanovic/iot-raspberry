55b9a8f083b0ea325e58d8dbeb34c8f07172d905
Ilija Marijanovic <ilija.marijanovic@gmx.at>
2020-09-25 11:42:47 +0200

    Update config.js

--- api/config/config.js
+++ api/config/config.js
@@ -1,3 +1,10 @@
+/**
+ * This Object contains different configuration options
+ *
+ * @module
+ * @property {Number} saltRounds - Number of salt rounds for hashing the password
+ */
+
 export default {
   saltRounds: 10,
 }
