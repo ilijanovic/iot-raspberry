b5b70c1e3762d614818b07e721d759c7d1e565d6
Ilija Marijanovic <ilija.marijanovic@gmx.at>
2020-09-25 11:22:37 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,6 +1,10 @@
 import express from 'express'
 import routes from './routes/index'
 let app = express()
+
+/*
+* Parses the incoming request Body. Needed for POST requests
+*/
 app.use(express.json())
 app.use(routes)
 
